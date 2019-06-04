7717db695e126310bf3149ae0d44665598e0ec75
Michael Clark <micl@umich.edu>
2019-06-03 22:36:34 -0400

    add tests for returned object

--- tests/testthat/test_summary_gamm.R
+++ tests/testthat/test_summary_gamm.R
@@ -4,8 +4,25 @@ ga_model = mgcv::gam(Reaction ~  Days + s(Subject, bs='re') + s(Days, Subject, b
                      data = lme4::sleepstudy,
                      method = 'REML')
 
-test_that('Works on single lme model', {
+ga_model_bin = mgcv::gam(Reaction > median(Reaction) ~  Days + s(Subject, bs='re') + s(Days, Subject, bs='re'),
+                         data = lme4::sleepstudy,
+                         family = binomial,
+                         method = 'REML')
+
+test_that('Works on single cluster lme model', {
   expect_type(summary_gamm(ga_model), 'list')
+})
+
+test_that('Returns df for vc', {
+  expect_s3_class(summary_gamm(ga_model)$vc, 'data.frame')
+})
+
+test_that('Returns df for fe', {
+  expect_s3_class(summary_gamm(ga_model)$fe, 'data.frame')
+})
+
+test_that('Works on single cluster lme model', {
+  expect_type(summary_gamm(ga_model_bin), 'list')
 })
 
 test_that('Returns invisible', {
