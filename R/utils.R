f1c9d06f8ac0e5492db9cbb2c6449f7bcfd7214c
Michael Clark <micl@umich.edu>
2019-06-19 08:10:19 -0400

    add visibly's gam plots

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+#' @importFrom visibly plot_gam plot_gam_2d plot_gam_3d plot_gam_by plot_gam_check
+
+#' @export
+visibly::plot_gam
+#' @export
+visibly::plot_gam_2d
+#' @export
+visibly::plot_gam_3d
+#' @export
+visibly::plot_gam_by
+#' @export
+visibly::plot_gam_check
